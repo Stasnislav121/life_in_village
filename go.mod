f6d88b207f0f9d5494606172e5cb2dcbe6fc027e
Avdokushin Stanislav <s_avdokushin@boxberry.ru>
2026-01-22 01:48:11 +0300

    add main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module life_in_village
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	village := Village{}
+
+	// Создаем жителей деревни
+	resident1 := &Resident{Name: "Алиса", Age: 30, Married: false, Alive: true, Events: []string{}}
+	resident2 := &Resident{Name: "Борис", Age: 40, Married: true, Alive: true, Events: []string{}}
+
+	// Создаем животных
+	animal1 := &Animal{Name: "Бобик", Age: 5, Type: "собака", Alive: true, Events: []string{}}
+	animal2 := &Animal{Name: "Мурка", Age: 3, Type: "кошка", Alive: true, Events: []string{}}
+
+	// Добавляем элементы в деревню
+	village.AddElement(resident1)
+	village.AddElement(resident2)
+	village.AddElement(animal1)
+	village.AddElement(animal2)
+
+	// Симуляция обновления деревни на несколько лет
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Год %d:\n", i+1)
+		village.UpdateAll()
+		fmt.Println(village.ShowAllInfo())
+	}
+}
